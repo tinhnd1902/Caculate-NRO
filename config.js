fcc130f0a9ca8ac4c769ba20a0ca4d2368372caa
Nguyễn Duy Tình <tinhnd1902@gmail.com>
2024-07-30 11:39:33 +0700

    init

---
+++ config.js
@@ -0,0 +1,40 @@
+export const traidat = {
+    namePlanet: 'Trái Đất',
+    infoThanLinhInitial : {
+        'ao' : "1600",
+        'quan' : "104000",
+        'gang' : "8800",
+        'giay' : "96000",
+        'nhan' : "16",
+    }
+}
+
+export const xayda = {
+    namePlanet: 'Xayda',
+    infoThanLinhInitial : {
+        'ao' : "1800",
+        'quan' : "96000",
+        'gang' : "9000",
+        'giay' : "92000",
+        'nhan' : "16",
+    }
+}
+
+export const namec = {
+    namePlanet: 'Namec',
+    infoThanLinhInitial : {
+        'ao' : "1700",
+        'quan' : "100000",
+        'gang' : "8600",
+        'giay' : "100000",
+        'nhan' : "16",
+    }
+}
+
+export const itemNames = {
+    ao: 'Áo',
+    quan: 'Quần',
+    gang: 'Găng',
+    giay: 'Giày',
+    nhan: 'Nhẫn'
+};
